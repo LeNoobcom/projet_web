f23fa704d6bd0327c7fa9ff7ee9e876b1c5b48d2
Senyuthe1 <y.bahammou05@gmail.com>
2026-01-27 14:45:43 +0100

    [Front]Ajout de nouvelles pages html

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// Espace pour le JavaScript à ajouter
 
+const genresParType = {
+    Film: ['Action', 'Drame', 'Comédie', 'Horreur', 'Science-Fiction', 'Aventure', 'Thriller'],
+    Serie: ['Action', 'Drame', 'Comédie', 'Horreur', 'Science-Fiction', 'Aventure', 'Thriller'],
+    Jeu: ['RPG', 'MMO', 'FPS', 'Stratégie', 'Aventure', 'Puzzle', 'Sport', 'Plateforme']
+};
+
+
+document.getElementById('TypeObjet').addEventListener('change', function () {
+    const typeSelected = this.value;
+    const selectGenre = document.getElementById('genre');
+
+    selectGenre.innerHTML = '<option selected disabled>Choisir un genre...</option>';
+
+    if (genresParType[typeSelected]) {
+        genresParType[typeSelected].forEach(genre => {
+            const option = document.createElement('option');
+            option.value = genre;
+            option.textContent = genre;
+            selectGenre.appendChild(option);
+        });
+    }
+});
